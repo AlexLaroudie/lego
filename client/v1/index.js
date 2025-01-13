330649074352abcae84021817a62ae46037692c9
AlexLaroudie <laroudiealexandre@gmail.com>
2025-01-13 19:54:43 +0100

    feat(total): compute number of deals

--- client/v1/index.js
+++ client/v1/index.js
@@ -58,6 +58,12 @@ console.log('Number of deals:', numberOfDeals);
 // 2. Log the variable
 // 3. Log how many shopping communities we have
 
+
+const communityNames = MY_FAVORITE_DEALERS.map(dealer => dealer.name);
+console.log('Community names:', communityNames);
+console.log('Number of communities:', communityNames.length);
+
+
 // 🎯 TODO 4: Sort by price
 // 1. Create a function to sort the deals by price
 // 2. Create a variable and assign it the list of sets by price from lowest to highest
